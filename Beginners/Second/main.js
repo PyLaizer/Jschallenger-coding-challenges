1ff3d70dc81b17998c7d660af216f6db46adf87b
Edward Eleazar <laizer2100@gmail.com>
2022-07-25 16:07:43 +0100

    Added 5 solved challenges

--- Beginners/Second/main.js
+++ Beginners/Second/main.js
@@ -23,3 +23,68 @@ function remlFunction(str){
     return result
 }
 document.getElementById("rem2").innerHTML = remlFunction("Laizer_AI")
+
+// 25/07/2022
+// A function that performs some basic arithmetic operations
+function aritFunction(a,b,c,d,e,f){
+    let result, x
+    x = (a + b) - c;
+    y = (x * d) / e;
+    result = y ** f;
+    return result;
+}
+
+document.getElementById("arit").innerHTML =  aritFunction(6,2,1,4,2,3)
+
+// A function that checks whether a string contains a string and concatenates
+
+function comFunction(a,b){
+let result = ""
+        if(a.includes(b) == true){
+            result = b.concat(a)
+            return result
+        }else{
+            result = a.concat(b)
+            return result
+        }
+}
+
+document.getElementById("com").innerHTML = comFunction("lips","s")
+
+
+// A function that checks if a number is even
+function evenFunction(x){
+ if(x % 2 == 0){
+    return true
+ }else{
+    return false
+ }
+}
+
+document.getElementById("even").innerHTML = evenFunction(24)
+
+// A function  that returns the no of times a string occurs
+function countFunction(x,y){
+    let result, count
+    count = 0;
+    for(let i of x ){
+        if(i == y){
+            count += 1
+        }
+    }
+    result = count;
+    return result
+}
+
+document.getElementById("count").innerHTML = countFunction("abacdefaa","a")
+
+// A function that checks if a number is a whole number
+function wholeFunction(x){
+   return Number.isInteger(x)
+}
+
+document.getElementById("whole").innerHTML = wholeFunction(45)
+
+
+
+
